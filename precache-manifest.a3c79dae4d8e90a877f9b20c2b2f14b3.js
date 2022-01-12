5719c1b91a7a58362f20f7f3371a2bb22ca080f8
asadullahjan <asadullah.jan@outlook.com>
2022-01-12 07:32:27 +0500

    Updates

---
+++ precache-manifest.a3c79dae4d8e90a877f9b20c2b2f14b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da73b550eba80a7218688dd7ec900664",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0f1c063abd624fbf3761",
+    "url": "/monster-rolodex/static/css/main.601532c1.chunk.css"
+  },
+  {
+    "revision": "48575e076b29a8ef0a72",
+    "url": "/monster-rolodex/static/js/2.c0a300b2.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monster-rolodex/static/js/2.c0a300b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102ffe9c1be55eb35ef1",
+    "url": "/monster-rolodex/static/js/3.26039094.chunk.js"
+  },
+  {
+    "revision": "0f1c063abd624fbf3761",
+    "url": "/monster-rolodex/static/js/main.85da6624.chunk.js"
+  },
+  {
+    "revision": "0db2fa8de08359dee2b7",
+    "url": "/monster-rolodex/static/js/runtime-main.2323d321.js"
+  }
+]);
